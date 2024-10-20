36f23d37129ce7dee8bd04cda25a1183c4f89652
imranhossen21 <imranhossen0586@gmail.com>
2024-10-20 17:08:07 +0600

    all done

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,10 +1,61 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import Country from "./country/Country";
+import './Countries.css'
 
 const Countries = () => {
+    const [country, setCountry] = useState([])
+    const [markedCountry, setMarkedCountry] = useState([])
+    const [fvFlags, setFvFlags] = useState([])
+
+
+    useEffect(() => {
+        const allCountryDataLoad = async () => {
+            const res = await fetch('https://restcountries.com/v3.1/all');
+            const data = await res.json()
+            setCountry(data)
+        }
+        allCountryDataLoad()
+    }, []);
+
+    const handleMarkVisited = country => {
+        const newMarkVisited = [...markedCountry, country]
+        setMarkedCountry(newMarkVisited)
+    }
+
+    const handleFvFlags = flags => {
+        const newFvFlags = [...fvFlags, flags]
+        setFvFlags(newFvFlags)
+    }
+    console.log(fvFlags);
+
+
     return (
         <div>
-            <h2>Countries: </h2>
-            <Country></Country>
+            <h2>Countries: {country.length}</h2>
+            <h3>Mark Visit Country: {markedCountry.length}</h3>
+            <div className="mark-country-container">
+                <ol className="marked-country">
+                    {
+                        markedCountry.map((markCountry, index) => <li key={index}>{markCountry.name.common}</li>)
+                    }
+                </ol>
+                <div className="fv-flags">
+                    {
+                        fvFlags.map((fvFlags, index) => <img key={index} src={fvFlags.png}></img>)
+                    }
+                </div>
+            </div>
+            <div className="country-container">
+                {
+                    country.map((country, index) => <Country
+                        key={index}
+                        country={country}
+                        handleMarkVisited={handleMarkVisited}
+                        handleFvFlags={handleFvFlags}>
+                    </Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/countries/country/Country.jsx
+++ src/components/countries/country/Country.jsx
@@ -1,7 +1,24 @@
-const Country = () => {
+import { useState } from 'react';
+import './Country.css'
+const Country = ({ country, handleMarkVisited, handleFvFlags }) => {
+    const { name, flags, area, region } = country;
+    const [visited, setVisited] = useState(false)
+
+
+    const handleVisitedCountry = () => {
+        setVisited(!visited)
+    }
+
     return (
-        <div>
-            <h2>Visited Countries: </h2>
+        <div className="country-card">
+            <h3>{name.common}</h3>
+            <img src={flags.png} alt="" />
+            <p className='details'><small>Area: {area}</small></p>
+            <p className='details'><small>Region: {region}{area}</small></p>
+            <p className='visited'><small>{visited ? "Tour complete the country." : "Got to the country Tour!"}</small></p>
+            <button onClick={handleVisitedCountry} className='btn'>{visited ? "Visited" : "Visit Now"}</button>
+            <button onClick={() => handleMarkVisited(country)} className='btn'>Mark Visit</button>
+            <button onClick={() => handleFvFlags(flags)} className='btn'>Add FV flags</button>
         </div>
     );
 };
